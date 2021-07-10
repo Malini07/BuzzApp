a4701ecc32978cb98404b2f5895066c8e0afc4bb
Malini07 <73117135+Malini07@users.noreply.github.com>
2021-07-10 15:43:04 +0530

    Add files via upload

---
+++ Facebook.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default class Facebook extends React.Component{
+  render(){
+  return (
+   <View style={{justifyContent:"center",flex:1,alignItems:"center"}}>
+   <Text>Facebook</Text>  
+    </View>
+  );
+  }
+}
